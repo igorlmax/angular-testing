745447a3534809791a145126c5f81f45b26330e5
igor <igorlmax@yahoo.com>
2019-02-23 18:55:46 +0100

    Add two new properties isAuthenticated and isAuthoriezed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-testing';
   user: any;
+  isAuthenticated: false;
+  isAuthorized: false;
 
     /**
      *
